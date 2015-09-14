1fb3e4a27764fe124022d455eb822e4cf55aae81
Emilio Wuerges <wuerges@gmail.com>
2015-09-14 18:04:38 -0300

    Greatly improved readability of Kuelmann

--- src/Kuelmann97.hs
+++ src/Kuelmann97.hs
@@ -1,51 +1,44 @@
 module Kuelmann97 where
 
+import Equivalence
+import Graph
+import BDD
+
+import Data.Graph.Inductive
+import Debug.Trace
+import qualified Data.Map as M
 
 -- | Checks Equivalence of circuits based on Kuelmann97
 equivKuelmann97 :: Checker
-equivKuelmann97 g1 g2 [o1] [o2] =
-    case checkExitPair g o1 o2 of
-      Just x -> Just x
-      Nothing -> equiv1 g M.empty (initialInputs g) o1 o2
+equivKuelmann97 g1 g2 [o1] [o2] = checkExitPair result o1 o2
     where g = g1 `union` g2
+          (_, _, result) = until (checkStop o1 o2) kuelmannStep (M.empty, initialInputs g, g)
 
 -- | Checks if both outputs are mapped to the same node in rg
+checkExitPair :: RG -> Int -> Int -> Maybe Bool
 checkExitPair rg o1 o2 = trace ("checkExitPair: " ++ show (o1, o2)) $
   case filter (\(n, v) -> elem o1 v && elem o2 v ) (labNodes rg) of
     [only] -> Just True
     _      -> Nothing
 
--- | main workhorse of Kuelmann97
-equiv1 :: RG -> M.Map BDD [Int] -> [Int] -> Int -> Int -> Maybe Bool
-equiv1 g bddmap inputs o1 o2 = trace ("equiv1: " ++ show inputs)  $
-  case cutGraph bddmap inputs g of
-    Just (g', bddmap', inputs') -> case checkExitPair g' o1 o2 of
-                             Just x -> Just x
-                             Nothing -> trace  ("equiv1 dentro: " ++ show inputs') $ equiv1 g' bddmap' inputs' o1 o2
-    Nothing -> Nothing
+-- | Checks if analysis should stop
+checkStop :: Int -> Int -> (M.Map BDD Int, [Int], RG) -> Bool
+checkStop o1 o2 (_, _, g) = maybe False (\_ -> True) (checkExitPair g o1 o2)
 
+-- | Performs one step of the iteration
+kuelmannStep :: (M.Map BDD Int, [Int], RG) -> (M.Map BDD Int, [Int], RG)
+kuelmannStep (m, [], g) = (m, [] , g)
+kuelmannStep (m, (i:is), g) = (m', is', g')
+  where g' = maybe g (\c -> if c == i then g else mergeNodes g i c) (M.lookup bdd m)
+        is' = maybe is (\ctx -> is ++ suc' ctx) (fst $ match i g')
+        m' = M.insertWith (\_ o -> o) bdd i m
+        bdd = createBDD g i
 
-
-
--- | cuts the inputs of a graph
-cutGraph :: M.Map BDD [Int] -> [Int] -> RG -> Maybe (RG, M.Map BDD [Int], [Int])
-cutGraph bddmap inputs g = trace ("cutGraph: " ++ show inputs)  $
-  case filter (\b -> length b > 1) (M.elems bdds) of
-    [] -> Just(g, bddmap', inputs'')
-      where inputs'' = nextFrontier inputs g g
-    merged -> Just (g', bddmap', inputs')
-      where g' = foldl mergeNodes g merged
-            inputs' = nextFrontier inputs g g'
-  where bdds = M.unionWith (++) bddmap (M.fromListWith (++) [(createBDD g i, [i]) | i <- inputs])
-        bddmap' = M.map (\(v:_) -> [v]) bdds
-
-
-nextFrontier :: [Int] -> RG -> RG -> [Int]
-nextFrontier ns g g' = S.toList $ S.fromList $ concat $ map (suc g) (filter (flip memberGraph g') ns)
-
-
+-- | Calculates the input nodes, that is, the nodes with input degree = 0
 initialInputs :: RG -> [Int]
 initialInputs g = [n | n <- nodes g, indeg g n == 0]
 
-mergeNodes :: RG -> [Int] -> RG
-mergeNodes g ns = error $ "uninplemented Merging nodes: " ++ show ns
+-- | Merges 2 nodes in the graph. The first one is mantained, the second one is removed
+-- | and all its sucessores are moved to the first one.
+mergeNodes :: RG -> Int -> Int -> RG
+mergeNodes g n1 n2 = error $ "uninplemented Merging nodes: " ++ show [n1, n2]
