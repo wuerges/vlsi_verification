528a337f61e9ae92acb5b655916cda60d520be47
Emilio Wuerges <wuerges@gmail.com>
2017-03-29 22:04:15 -0300

    union find ok

--- src/BDDMinimizer.hs
+++ src/BDDMinimizer.hs
@@ -17,42 +17,41 @@ newtype D = D Int deriving (Eq, Ord)
 minD :: D -> D -> D
 minD (D a) (D b) = D $ min a b
 
-type MinT s = EquivT s D Node KS
+type Min s = EquivM s D Node
 
-
-runMinT :: MinT s a -> KS a
-runMinT op = runEquivT (\n -> D n) minD op
-
-leader :: Node -> MinT s D
+leader :: Node -> Min s D
 leader n = classDesc n
 
-minimize :: T -> (T, [(Node, Node)])
-minimize = undefined
+minimize :: T -> Min s (T, [(Node, Node)])
+minimize t =
+  let ls = layers t
+   in do mapM_ (minimizeLayer1 t >> minimizeLayer2 t) ls
+         return (t, [])
 
-minimizeLayer :: [Node] -> MinT s ()
-minimizeLayer ns = do
-  mapM_ minimizeLayer1 ns
 
-minimizeLayer1 :: Node -> MinT s ()
-minimizeLayer1 n = do
-  t <- lift getT
+minimizeLayer1 :: T -> [Node] -> Min s ()
+minimizeLayer1 t ns = do
+  mapM_ (minimizeLayer1n t) ns
+
+minimizeLayer1n :: T -> Node -> Min s ()
+minimizeLayer1n t n = do
   let (z, o) = getSons t n
   when (gelem n t && outdeg t n > 0) $ do
     e <- equivalent z o
     when e $ E.equate n z
 
 
-getEqSons :: Node -> MinT s (Node, (D, D))
-getEqSons n = do
-  (z, o) <- lift $ flip getSons n <$> getT
-  zc <- leader z
-  oc <- leader o
-  return (n, (zc, oc))
+getEqSons :: T -> Node -> Min s (Node, (D, D))
+getEqSons t n =
+  let (z, o) = getSons t n
+   in do zc <- leader z
+         oc <- leader o
+         return (n, (zc, oc))
 
-minimizeLayer2 :: [Node] -> MinT s ()
-minimizeLayer2 ns = do
-  t <- lift getT
+minimizeLayer2 :: T -> [Node] -> Min s ()
+minimizeLayer2 t ns =
   let ns' = filter (flip gelem t) ns
-  ns'' <- mapM getEqSons ns'
-  let gs' = map (map fst) (sortAndGroupBy snd ns'')
-  mapM_ equateAll gs'
+   in do ns'' <- mapM (getEqSons t) ns'
+         let gs' = map (map fst) (sortAndGroupBy snd ns'')
+         mapM_ equateAll gs'
+
